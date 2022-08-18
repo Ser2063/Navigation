cc37949909ab54d3e323919281a1ca2b57c0d4e2
Ser2063 <97412512+Ser2063@users.noreply.github.com>
2022-08-18 12:06:45 +0300

    Revert "Feature/iosui 1.9"

--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -43,4 +43,16 @@ class PostViewController: UIViewController {
         self.present(myInfoViewController, animated: true, completion: nil)
 
     }
+
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
 }
